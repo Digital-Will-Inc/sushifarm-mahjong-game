6f8e68be7d32df815eafac7a25e045aa906c7ca6
bimaswara <bimaswaraadam@gmail.com>
2025-06-09 14:00:40 +0700

    feat : set up next config to get pure html/css/js

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   devIndicators: false,
+  output: 'export',
+  assetPrefix: './',
+  basePath: '',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  experimental: {
+    esmExternals: false
+  }
 }
 
 export default nextConfig;
